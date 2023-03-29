ed5b0e9acec01d1b90318b856708540ab2226e9b
Yue Yu <yue.yu@superfish.me>
2023-03-29 14:01:51 -0400

    feat(visitor-badge): support visitor badge

--- app/Main.hs
+++ app/Main.hs
@@ -5,16 +5,20 @@ import Data.Function ((&))
 import qualified Data.Text.IO as TIO
 import qualified Data.Text.Lazy as TL
 import WakaHS.IO.Env (barStyle, input, output)
+import WakaHS.IO.Github (currentUserLogin)
 import WakaHS.IO.WeeklySummary (weeklySummary)
+import WakaHS.Render.Component.VisitorBadge (VisitorBadge (VisitorBadge))
 import WakaHS.Render.Component.WeeklySummary (WeeklySummary (WeeklySummary))
-import WakaHS.Render.Render (RenderComponent (Wrap), renderFile)
+import WakaHS.Render.Render (RenderComponent (Wrap), Renderable, renderFile)
 
 renders :: IO [RenderComponent]
 renders =
   sequence
-    [ render $ liftA2 WeeklySummary barStyle weeklySummary
+    [ render $ liftA2 WeeklySummary barStyle weeklySummary,
+      render $ fmap VisitorBadge currentUserLogin
     ]
   where
+    render :: Renderable a => IO a -> IO RenderComponent
     render = fmap Wrap
 
 main :: IO ()
---
+++ lib/WakaHS/Render/Component/VisitorBadge.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module WakaHS.Render.Component.VisitorBadge where
+
+import Data.Aeson (Key)
+import qualified Data.Text.Lazy as TL
+import WakaHS.Render.Render (Renderable (name, render))
+
+tmplf :: TL.Text -> TL.Text
+tmplf id' = "[!visitors](https://visitor-badge.glitch.me/badge?page_id=" <> id' <> "." <> id' <> "&left_color=green&right_color=red)"
+
+data VisitorBadge where
+  VisitorBadge :: TL.Text -> VisitorBadge
+
+instance Renderable VisitorBadge where
+  name :: VisitorBadge -> Key
+  name _ = "VisitorBadge"
+
+  render :: VisitorBadge -> TL.Text
+  render (VisitorBadge id') = tmplf id'
