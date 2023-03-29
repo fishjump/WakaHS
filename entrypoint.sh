a218e944124c9792f953166e3e4efbcd8cd1f9ea
Yue Yu <yue.yu@superfish.me>
2023-03-29 00:40:45 -0400

    fix(action): add github token to local git

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@
 git config --global user.name "wakahs-bot"
 git config --global user.email "wakahs-bot@noreply.github.com"
 
+# Configure Git to use the GitHub token for authentication
+git config --global credential.helper '!f() { echo "password=${INPUT_GH_TOKEN}"; }; f'
+
 git --git-dir=/github/workspace/.git --work-tree=/github/workspace add README.md
 git --git-dir=/github/workspace/.git --work-tree=/github/workspace commit -m "update(README.md): wakahs-bot update"
 git --git-dir=/github/workspace/.git --work-tree=/github/workspace push
