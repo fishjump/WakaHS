d53eab283ff9011f248522b0aa239c74358ad4fd
Yue Yu <yue.yu@superfish.me>
2023-03-29 21:50:39 -0400

    fix(visitor-badge): remove the title because frequent visit may cause this resource temporarily unavailable, in this case, no title can hide this image

--- lib/WakaHS/Render/Component/VisitorBadge.hs
+++ lib/WakaHS/Render/Component/VisitorBadge.hs
@@ -9,7 +9,7 @@ import qualified Data.Text.Lazy as TL
 import WakaHS.Render.Render (Renderable (name, render))
 
 tmplf :: TL.Text -> TL.Text
-tmplf id' = "![visitors](https://visitor-badge.glitch.me/badge?page_id=" <> id' <> "." <> id' <> "&left_color=gray&right_color=red)"
+tmplf id' = "![](https://visitor-badge.glitch.me/badge?page_id=" <> id' <> "." <> id' <> "&left_color=gray&right_color=red)"
 
 data VisitorBadge where
   VisitorBadge :: TL.Text -> VisitorBadge
