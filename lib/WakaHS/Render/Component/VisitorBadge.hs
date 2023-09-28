7818fc55334884ae899a00307d0782af4f9b07c5
Yue Yu <yue.yu@fism.io>
2023-09-27 20:57:09 -0400

    fix: counter api is dead, switch api

--- lib/WakaHS/Render/Component/VisitorBadge.hs
+++ lib/WakaHS/Render/Component/VisitorBadge.hs
@@ -9,7 +9,7 @@ import qualified Data.Text.Lazy as TL
 import WakaHS.Render.Render (Renderable (name, render))
 
 tmplf :: TL.Text -> TL.Text
-tmplf id' = "![](https://shields.io/badge/dynamic/json?color=blue&label=Visitors&query=value&url=https://api.countapi.xyz/hit/" <> id' <> "." <> id' <> ")"
+tmplf id' = "![](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2F" <> id' <> "%2F" <> id' <> "&countColor=%232ccce4&style=flat)"
 
 data VisitorBadge where
   VisitorBadge :: TL.Text -> VisitorBadge
